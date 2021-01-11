367244cde032769f4cefac37adaccc5f1288594d
indresh-cedcoss <indreshhyadav@cedcoss.com>
2021-01-11 13:16:00 +0530

    test

--- hello.php
+++ hello.php
@@ -8,6 +8,17 @@
 	<body>
 		<h2>Welcome to the cedcoss</h2>
 		<h3>Hey, This is a code done by me ,Snehlata.</h3>
+		<h3>Hey, This is a code done by INDRESH.</h3>
+
 	</body>
 	<h1>chages by ind-test</h1>
 </html>>
+
+
+<?php 
+function test_pull()
+{
+	print('TEST');
+}
+
+?>
